ee3a1e16b78b36783276ce524e117666215f8760
jiangbin <jiangbin@coohua.com>
2017-04-09 17:38:38 +0800

    提交from miaohuiqin

--- Hello.java
+++ Hello.java
@@ -2,5 +2,8 @@ public class Hello{
     public static void main(String[]args){
         System.out.println("miaohuiqin I love u");
         System.out.println("We will marry this year");
+        
+        System.out.println("Hi i am miaohuiqin");
+        System.out.println("I am not sure loove you");
     }
 }
