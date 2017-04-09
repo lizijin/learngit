9afbb975af7e6c93f175d0dfa93b7d0b026a7a14
jiangbin <jiangbin@coohua.com>
2017-04-09 17:44:05 +0800

    添加约会方法

--- Hello.java
+++ Hello.java
@@ -6,4 +6,11 @@ public class Hello{
         System.out.println("Hi i am miaohuiqin");
         System.out.println("I am not sure loove you");
     }
+    
+    /**
+     *添加约会方法
+     **/
+    public void date(){
+        
+    }
 }
